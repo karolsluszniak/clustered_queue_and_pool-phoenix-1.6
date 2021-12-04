530800b04a0c29f33de0fb6bf7629b2d29d843c4
Karol Słuszniak <ksluszniak@gmail.com>
2021-12-04 16:57:02 +0100

    Show avatar upload live errors

--- lib/outer_web/views/user_settings_view.ex
+++ lib/outer_web/views/user_settings_view.ex
@@ -1,3 +1,6 @@
 defmodule OuterWeb.UserSettingsView do
   use OuterWeb, :view
+
+  defp avatar_error_to_string(:too_large), do: "image file below 500KB"
+  defp avatar_error_to_string(:not_accepted), do: "image file in JPEG format"
 end
